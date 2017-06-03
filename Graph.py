822d3d8658a713af96c4f04e431ab4ff0c8f618c
MarlonCosta <marlon.costa@ieee.org>
2017-06-02 23:31:31 -0300

    Corrigido bug de contagem no seguinte caso:
    Vértices com mais de uma letra + existência de arestas inversas e correspondentes.

--- Graph.py
+++ Graph.py
@@ -1,10 +1,11 @@
+import pandas as pd
+
 class Graph:
-    def __init__(self, vertices=[], edges={}):
+    def __init__(self, vertices=[], edges=[]):
         '''
         Português: Constrói um objeto Graph. Se houver um vértice ou aresta inválido(a), uma exceção é chamada
-        :param vertices: Uma lista com os vértices do grafo. Os vértices não devem conter mais que uma letra
-        (para manter a formatação).
-        :param edges: Um dicionário com as arestas no formato nome(A-B), sendo A e B vértices válidos'''
+        :param vertices: Uma lista com os vértices do grafo.
+        :param edges: Uma lista com as arestas no formato nome(A-B), sendo A e B vértices válidos'''
 
         self.separator = '-'
         self.vertices = vertices
@@ -14,7 +15,7 @@ class Graph:
             if not self.checkvertix(vertix):
                 raise ValueError("Invalid Vertix: " + vertix)
 
-        for edge in edges.values():
+        for edge in edges:
             if not self.checkedge(edge):
                 raise ValueError("Invalid edge: " + edge)
 
@@ -32,7 +33,8 @@ class Graph:
             return False
         if edge.startswith(self.separator) or edge.endswith(self.separator):
             return False
-        if edge[0] not in self.vertices or edge[-1] not in self.vertices:
+        if edge[:edge.index(self.separator)] not in self.vertices \
+                or edge[edge.index(self.separator) + 1:] not in self.vertices:
             return False
         return True
 
@@ -51,12 +53,13 @@ class Graph:
         for vertix in self.vertices:
             for aux_vertix in self.vertices:
                 comb = vertix + self.separator + aux_vertix
+                rev_comb = aux_vertix + self.separator + vertix
                 x = self.vertices.index(vertix)
                 y = self.vertices.index(aux_vertix)
-                if comb in self.edges.values():
-                    matrix[x][y] += list(self.edges.values()).count(comb)
-                    if comb != comb[::-1]:
-                        matrix[x][y] += list(self.edges.values()).count(comb[::-1])
+                if comb in self.edges:
+                    matrix[x][y] += list(self.edges).count(comb)
+                    if comb != rev_comb:
+                        matrix[x][y] += list(self.edges).count(rev_comb)
 
         for i in range(len(matrix)):
             for j in range(len(matrix)):
@@ -68,17 +71,10 @@ class Graph:
         return matrix
 
     def __str__(self):
-        matrix = self.generatematrix()
-        out = '     '
-        for vertix in self.vertices:
-            out += vertix + '    '
-
-        out += '\n'
-
-        for line in matrix:
-            out += self.vertices[matrix.index(line)] + '  ' + str(line) + '\n'
-        return out
+        '''Converte o grafo em um DataFrame para melhor exibição'''
+        df = pd.DataFrame(self.generatematrix(), index=self.vertices, columns=self.vertices)
+        return str(df)
 
 
-grafo = Graph(["A", "B", "C", "D", "E"], {"a1": "A-C", "a2": "B-B", "a3": "A-A", "a4": "A-B"})
+grafo = Graph(["Iri", "neu"], ["Iri-neu", "neu-Iri"])
 print(grafo)
