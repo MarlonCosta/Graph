4170eea743f4b2a82a854f03f74ccc572a6a3ea4
MarlonCosta <marlon.costa@ieee.org>
2017-06-07 18:28:28 -0300

    Versão quase final

--- Graph.py
+++ Graph.py
@@ -11,12 +11,18 @@ class Graph:
         self.separator = '-'
         self.vertices = vertices
         self.edges = edges
+        self.matrix = self.generatematrix()
 
-        for vertix in vertices:
-            self.checkvertix(vertix)
+        for vertex in vertices:
+            self.checkvertex(vertex)
 
         for edge in edges:
             self.checkedge(edge)
+
+    def __str__(self):
+        '''Converte o grafo em um DataFrame para melhor exibição'''
+        df = pd.DataFrame(self.generatematrix(), index=self.vertices, columns=self.vertices)
+        return str(df)
 
     def getedges(self):
         '''Retorna as arestas'''
@@ -36,17 +42,17 @@ class Graph:
 
         if edge[:edge.index(self.separator)] not in self.vertices \
                 or edge[edge.index(self.separator) + 1:] not in self.vertices:
-            raise ValueError("Invalid edge " + edge + " vertix not found")
+            raise ValueError("Invalid edge " + edge + " vertex not found")
 
-    def checkvertix(self, vertix):
+    def checkvertex(self, vertex):
         '''Checa se um vértice é válido'''
-        if self.separator in vertix:
-            raise ValueError("Invalid Vertix " + vertix + ": vertix contains separator character")
-        if vertix == "":
-            raise ValueError("Invalid Vertix: Empty value")
+        if self.separator in vertex:
+            raise ValueError("Invalid vertex " + vertex + ": vertex contains separator character")
+        if vertex == "":
+            raise ValueError("Invalid vertex: Empty value")
         for v in self.vertices:
             if self.vertices.count(v) > 1:
-                raise ValueError("Invalid Vertix: Two or more inputs of the same vertix " + v)
+                raise ValueError("Invalid vertex: Two or more inputs of the same vertex " + v)
 
     def generatematrix(self):
         '''Gera a matriz que representa o grafo'''
@@ -54,12 +60,12 @@ class Graph:
         for v in self.vertices:
             matrix.append([0] * len(self.vertices))
 
-        for vertix in self.vertices:
-            for aux_vertix in self.vertices:
-                comb = vertix + self.separator + aux_vertix
-                rev_comb = aux_vertix + self.separator + vertix
-                x = self.vertices.index(vertix)
-                y = self.vertices.index(aux_vertix)
+        for vertex in self.vertices:
+            for aux_vertex in self.vertices:
+                comb = vertex + self.separator + aux_vertex
+                rev_comb = aux_vertex + self.separator + vertex
+                x = self.vertices.index(vertex)
+                y = self.vertices.index(aux_vertex)
                 if comb in self.edges:
                     matrix[x][y] += list(self.edges).count(comb)
                     if comb != rev_comb:
@@ -74,30 +80,117 @@ class Graph:
 
         return matrix
 
-    def checkbetweenvertices(self, vertix1, vertix2):
+    def checkbetweenvertices(self, vertex1, vertex2):
+        '''Checa a existência de uma aresta entre os 2 vértices entre os parâmetros
+        :param vertex1: Primeiro vértices que compoe a aresta a ser checada
+        :param vertex2: Segundo vértice que compoe a aresta a ser checada'''
 
-        if vertix1 + "-" + vertix2 in self.edges or vertix2 + "-" + vertix1 in self.edges:
+        if vertex1 + "-" + vertex2 in self.edges or vertex2 + "-" + vertex1 in self.edges:
             return True
         else:
             return False
 
     def nonadjacentpairs(self):
+        '''Retorna uma lista com os pares não adjacentes'''
         pares = []
-        matrix = self.generatematrix()
-        for line in range(len(matrix)):
-            for column in range(len(matrix[line])):
-                if matrix[line][column] == 0:
-                    saida = self.vertices[matrix.index(line)] + self.separator + self.vertices[matrix.index(column)]
+
+        for line in range(len(self.matrix)):
+            for column in range(len(self.matrix[line])):
+                if self.matrix[line][column] == '0':
+                    saida = self.vertices[line] + self.separator + self.vertices[column]
                     pares.append(saida)
+        if not pares:
+            return None
         return pares
 
+    def getloops(self):
+        ''' Retorna uma lista com os vértices que possuem loop (adjacentes a eles mesmos), caso existam'''
+        loops = []
 
-    def __str__(self):
-        '''Converte o grafo em um DataFrame para melhor exibição'''
-        df = pd.DataFrame(self.generatematrix(), index=self.vertices, columns=self.vertices)
-        return str(df)
+        for line in range(len(self.matrix)):
+            for column in range(len(self.matrix[line])):
+                if self.matrix[line][column] != self.separator and int(self.matrix[line][column]) > 0 and self.vertices[
+                    line] == \
+                        self.vertices[column]:
+                    loops.append(self.vertices[line])
+        if not loops:
+            return None
+        return loops
+
+    def getparalleledges(self):
+        '''Retorna uma lista com as arestas que tem alguma aresta paralela, caso existam'''
+        parallel_edges = []
+
+        for line in range(len(self.matrix)):
+            for column in range(len(self.matrix[line])):
+                if self.matrix[line][column] != self.separator:
+                    if int(self.matrix[line][column]) > 1:
+                        parallel_edges.append(self.vertices[line] + self.separator + self.vertices[column])
+        if not parallel_edges:
+            return None
+        return parallel_edges
+
+    def edgesin(self, vertex):
+        '''Retorna as arestas incidentes no vértice
+        :param vertex: Vértice no qual será conferida a incidência de arestas'''
+
+        if vertex not in self.vertices:
+            raise NameError("O grafo não contém o vértice inserido")
+
+        edges = []
+
+        for edge in self.edges:
+            if vertex in edge:
+                edges.append(edge)
+
+        if not edges:
+            return None
+        return edges
+
+    def degree(self, vertex):
+        '''Retorna o grau do vérticie posto como parâmetro
+        :param vertex: Vértice que terá seu grau calculado'''
+
+        if vertex not in self.vertices:
+            raise NameError("O grafo não contém o vértice inserido")
+
+        degree = 0
+
+        for edge in self.edges:
+            if vertex in edge:
+                degree += 1
+
+        return degree
+
+    def iscomplete(self):
+        '''Retorna um booleano indicando se o grafo é completo'''
+
+        # Separar as arestas únicas, ignorando paralelas ou inversas
+        uniqueEdges = []
+
+        for edge in self.edges:
+            head = edge[:edge.index(self.separator)]
+            tails = edge[edge.index(self.separator) + 1:]
+            rev_edge = tails + self.separator + head
+            if rev_edge in uniqueEdges:
+                continue
+            else:
+                uniqueEdges.append(edge)
+
+        n = len(self.vertices)
+
+        if len(uniqueEdges) == n * (n - 1) / 2:
+            return True
+        else:
+            return False
 
 
-g = Graph(['J', 'C', 'E', 'P', 'M', 'T', 'Z'], ['J-C', 'C-E', 'C-E', 'C-P', 'C-P', 'C-M', 'C-T', 'M-T', 'T-Z'])
+g = Graph(['J', 'C', 'E', 'P', 'M', 'T', 'Z'], ['J-C', 'C-E', 'C-E', 'C-E', 'C-P', 'C-M', 'C-T', 'M-T', 'T-Z'])
+
 print(g)
-print(g.nonadjacentpairs())
+print(g.nonadjacentpairs())  # Questão 3.a
+print(g.getloops())  # Questão 3.b (alteração: retorna os vértices onde existem loops em vez de True)
+print(g.getparalleledges())  # Questão 3.c (alteração: retorna quais arestas tem outras arestas paralelas)
+print(g.degree("C"))  # Questão 3.d
+print(g.edgesin("M"))  # Questão 3.e
+print(g.iscomplete())  # Questão 3.f
